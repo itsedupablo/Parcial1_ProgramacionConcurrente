a01f1e9337e4512efc960fbe7eafcae853e27fd3
itsedupablo <158079190+itsedupablo@users.noreply.github.com>
2024-10-08 16:44:32 +0200

    Add files via upload

---
+++ fabrica-galton/src/main/java/com/edupablo/parcial1/fabrica_galton/FabricaGaltonApplication.java
@@ -0,0 +1,13 @@
+package com.edupablo.parcial1.fabrica_galton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaGaltonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaGaltonApplication.class, args);
+	}
+
+}
---
+++ fabrica-galton/src/test/java/com/edupablo/parcial1/fabrica_galton/FabricaGaltonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edupablo.parcial1.fabrica_galton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaGaltonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
